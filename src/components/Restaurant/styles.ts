ca70687b7b77390f08a63a2b66a4cfbf41ef15d4
pedrogrocha33 <143932641+pedrogrocha33@users.noreply.github.com>
2024-08-13 22:26:20 -0300

    first commit

---
+++ src/components/Restaurant/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+import { breakpoints, cores } from '../../styles'
+
+export const Card = styled.div`
+  background-color: ${cores.branco};
+  border: solid 1px ${cores.vermelho};
+
+  .ContainerTop {
+    display: flex;
+    justify-content: space-between;
+    padding: 8px;
+  }
+  position: relative;
+`
+export const Photo = styled.img`
+  width: 100%;
+  height: 217px;
+  object-fit: cover;
+`
+export const Title = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+`
+export const Description = styled.p`
+  font-size: 14px;
+  font-weight: regular;
+  margin-right: 8px;
+  margin-left: 8px;
+`
+export const Rate = styled.div`
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 8px;
+    margin-right: 8px;
+  }
+  img {
+    padding-top: 6px;
+    width: 21px;
+    height: 31px;
+  }
+  display: flex;
+`
+export const Categories = styled.div`
+  position: absolute;
+  top: 0;
+  right: 16px;
+`
+
